0f9b036fb8f5c4b2875afc92ee41389caf9daa20
Vladyslav Lozhechnyk <lozhechnik.vladislav@gmail.com>
2016-08-12 21:14:02 +0300

    Smar year filter

--- frontend/js/controllers/items_list_filters.js
+++ frontend/js/controllers/items_list_filters.js
@@ -5,6 +5,9 @@ module.exports = function ($scope, $rootScope, $resource, autoData) {
     $scope.brands = autoData.brands;
     $scope.models = [];
 
+    var originalYearFrom = null,
+        originalYearTo = null;
+
     $scope.filterByBrand = function () {
         $scope.models = autoData.models.filter(function (model) {
             return $scope.filters.brand && model.brand_id === $scope.filters.brand.id;
@@ -17,7 +20,23 @@ module.exports = function ($scope, $rootScope, $resource, autoData) {
         $scope.updateFilters();
     };
 
-    $scope.updateFilters = function () {
+    $scope.updateFilters = function (sender) {
+        if (sender == 'yearFrom') {
+            originalYearFrom = $scope.filters.yearFrom;
+
+            $scope.filters.yearTo = originalYearTo;
+            if (originalYearFrom && originalYearTo && originalYearTo < originalYearFrom) {
+                $scope.filters.yearTo = $scope.filters.yearFrom;
+            }
+        }
+        if (sender == 'yearTo') {
+            originalYearTo = $scope.filters.yearTo;
+
+            $scope.filters.yearFrom = originalYearFrom;
+            if (originalYearFrom && originalYearTo && originalYearTo < originalYearFrom) {
+                $scope.filters.yearFrom = originalYearTo;
+            }
+        }
         $rootScope.$broadcast('itemsListFiltersChange', $scope.filters);
     };
 };
