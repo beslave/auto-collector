5b1ad88f35b141dd91d2460c01008be1a7fe80bf
Vladyslav Lozhechnyk <lozhechnik.vladislav@gmail.com>
2016-08-15 20:28:24 +0300

    Add advertisements table ordering

--- frontend/js/controllers/model.js
+++ frontend/js/controllers/model.js
@@ -1,11 +1,29 @@
 'use strict';
 
-module.exports = function ($scope, $http, $location) {
+var TABLE_ORDERING_STORE_KEY = 'advertisements-table-ordering';
+
+module.exports = function ($scope, $http, $location, store) {
     angular.element(document.body).addClass('modal-open');
+
     $http.get($location.absUrl()).then(function(response) {
         var data = response.data;
         $scope.brand = data.brand;
         $scope.model = data.model;
         $scope.advertisements = data.advertisements;
     });
+
+    $scope.ordering = store.get(TABLE_ORDERING_STORE_KEY) || {
+        by: 'price',
+        reverse: false
+    };
+
+    $scope.setOrderBy = function(attr) {
+        if ($scope.ordering.by === attr) {
+            $scope.ordering.reverse = !$scope.ordering.reverse;
+        } else {
+            $scope.ordering.by = attr;
+            $scope.ordering.reverse = false;
+        }
+        store.set(TABLE_ORDERING_STORE_KEY, $scope.ordering);
+    };
 };
