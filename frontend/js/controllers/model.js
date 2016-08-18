481b76ef090e8ba051d40c011e5d059242b657d1
Vladyslav Lozhechnyk <lozhechnik.vladislav@gmail.com>
2016-08-18 08:09:00 +0300

    Filter advertisements in modal

--- frontend/js/controllers/model.js
+++ frontend/js/controllers/model.js
@@ -2,14 +2,17 @@
 
 var TABLE_ORDERING_STORE_KEY = 'advertisements-table-ordering';
 
-module.exports = function ($scope, $http, $location, store) {
+module.exports = function ($scope, $filter, $http, $location, store, autoData) {
     angular.element(document.body).addClass('modal-open');
+    $scope.advertisements = [];
+    $scope.filteredAdvertisements = [];
 
     $http.get($location.absUrl()).then(function(response) {
         var data = response.data;
         $scope.brand = data.brand;
         $scope.model = data.model;
         $scope.advertisements = data.advertisements;
+        updateFilters();
     });
 
     $scope.ordering = store.get(TABLE_ORDERING_STORE_KEY) || {
@@ -30,4 +33,34 @@ module.exports = function ($scope, $http, $location, store) {
     $scope.closeModal = function () {
         $location.path('/');
     };
+
+    function updateFilters () {
+        var filtered = $scope.advertisements;
+
+        if (!autoData.filters.isNew) {
+            filtered = $filter('filter')(filtered, {is_new: false});
+        }
+
+        if (!autoData.filters.isUsed) {
+            filtered = $filter('filter')(filtered, {is_new: true});
+        }
+
+        if (autoData.filters.yearFrom) {
+            filtered = filtered.filter(function (adv) {
+                return adv.year >= autoData.filters.yearFrom;
+            });
+        }
+
+        if (autoData.filters.yearTo) {
+            filtered = filtered.filter(function (adv) {
+                return adv.year <= autoData.filters.yearTo;
+            });
+        }
+
+        $scope.filteredAdvertisements = filtered;
+        $scope.$applyAsync();
+    }
+
+    $scope.$on('autoDataChanged', updateFilters);
+    updateFilters();
 };
